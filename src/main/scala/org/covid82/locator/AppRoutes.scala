0942321aa4e62f8408a5eed6006e48397a28f084
covid82 <denis.zjukov@gmail.com>
2020-06-16 22:47:12 +0100

    LOCATOR-3: Implemented apiRoute that serves country lookup requests

--- src/main/scala/org/covid82/locator/AppRoutes.scala
+++ src/main/scala/org/covid82/locator/AppRoutes.scala
@@ -13,4 +13,45 @@ object AppRoutes {
         Ok("Ok")
     }
   }
+
+  import scala.util.Try
+  import java.net.InetAddress
+
+  object InetAddressVar {
+    def unapply(str: String): Option[String] = if (str.isEmpty) None else {
+      Try(InetAddress.getByName(str)).map(_ => str).toOption
+    }
+  }
+
+  import cats.effect.{Effect, ContextShift, Timer}
+
+  def apiRoutes[F[_] : Effect : ContextShift : Timer](
+    ripeService: RipeService[F], registry: IpRegistry
+  ): HttpRoutes[F] = {
+    val dsl = new Http4sDsl[F] {}
+    import dsl._
+    import org.http4s.circe.CirceEntityEncoder._
+    import io.circe.generic.auto._
+    HttpRoutes.of[F] {
+      case GET -> Root / "api" / "locate" / InetAddressVar(ip) => ripeService.find(ip)(registry) match {
+        case Some(country) => Ok(Location(ip, country))
+        case None => NotFound("Not Found")
+      }
+    }
+  }
+
+  import cats.Monad
+  import cats.effect.{Blocker, Sync}
+  import org.http4s.{StaticFile, Header}
+
+  def staticFilesRoute[F[_] : Sync : Monad : ContextShift](blocker: Blocker): HttpRoutes[F] = {
+    val dsl = new Http4sDsl[F] {}
+    import dsl._
+    HttpRoutes.of[F] {
+      case request @ GET -> Root / "api" / "locate" =>
+        StaticFile.fromResource("index.html", blocker, Some(request))
+          .map(_.putHeaders(Header("Content-Type", "text/html; charset=UTF-8")))
+          .getOrElseF(NotFound("Not Found"))
+    }
+  }
 }
--- src/main/scala/org/covid82/locator/AppServer.scala
+++ src/main/scala/org/covid82/locator/AppServer.scala
@@ -1,18 +1,22 @@
 package org.covid82.locator
 
-import cats.effect.{ConcurrentEffect, ContextShift, Sync, Timer}
+import cats.effect.{Blocker, ConcurrentEffect, ContextShift, Timer}
 import fs2.Stream
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.implicits._
+import cats.syntax.semigroupk._
+import AppRoutes._
+import cats.Applicative
 
 object AppServer {
-  def stream[F[_] : ConcurrentEffect : ContextShift : Timer](config: FtpConfig): Stream[F, Nothing] = {
+  def stream[F[_] : ConcurrentEffect : ContextShift : Timer](config: FtpConfig, blocker: Blocker): Stream[F, Nothing] = {
     for {
-      registry <- RipeService[F](config).read
-      _ <- Stream.eval_(Sync[F].delay(registry.take(10).foreach(println)))
+      ripeService <- Stream.eval(Applicative[F].pure(RipeService[F](config)))
+      registry <- ripeService.read
+      routes = monitoringRoutes[F] <+> staticFilesRoute(blocker) <+> apiRoutes[F](ripeService, registry)
       exitCode <- BlazeServerBuilder[F]
         .bindHttp(8080, "0.0.0.0")
-        .withHttpApp(AppRoutes.monitoringRoutes[F].orNotFound)
+        .withHttpApp(routes.orNotFound)
         .serve
     } yield exitCode
   }.drain
